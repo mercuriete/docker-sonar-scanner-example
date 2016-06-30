b5405b23958d10546660dd3e25be73807a814daf
Abel Garcia Dorta <abel.garcia.external@atos.net>
2016-06-30 12:13:20 +0100

    creating an example of java project to be analized with sonar scanner

---
+++ sonar-scanner/proyecto/HelloWorld.java
@@ -0,0 +1,13 @@
+
+public class HelloWorld {
+	
+	private HelloWorld() {
+
+	}
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+
+}
